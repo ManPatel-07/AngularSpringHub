1ca45167c24f5a1bcb5f828b305c886cafe2e2ea
ManPatel-07 <patelman7720@gmail.com>
2024-11-12 14:30:33 +0530

    insteed of use httpclient use feign client it's easy to use ...
    insteed of use httpclient use feign client it's easy to use ...

--- MicroServices_WorkSpace/inventory-service/src/main/java/org/mm/inventory_service/controller/ProductController.java
+++ MicroServices_WorkSpace/inventory-service/src/main/java/org/mm/inventory_service/controller/ProductController.java
@@ -2,6 +2,8 @@ package org.mm.inventory_service.controller;
 
 import java.util.List;
 
+import org.mm.inventory_service.clients.OrdersFeignClient;
+import org.mm.inventory_service.entity.OrderRequest;
 import org.mm.inventory_service.entity.Product;
 import org.mm.inventory_service.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +13,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
+@Slf4j
 @RequestMapping("/products")
 public class ProductController
 {
@@ -29,15 +37,15 @@ public class ProductController
 	@Autowired
 	private RestClient restclient;
 	
+	@Autowired
+	private OrdersFeignClient ordersFeignClient;
+	
 	@GetMapping(path = "/fetchOrders")
-	public String fetchOrders()
+	public String fetchOrders(HttpServletRequest httpServletRequest)
 	{
-		ServiceInstance orderService = discoveryClient.getInstances("order-service").getFirst();
+		log.info(httpServletRequest.getHeader("X-Pqr-Header"));
 		
-		return restclient.get()
-				.uri(orderService.getUri()+ "/api/v1/orders/helloOrders")
-				.retrieve()
-				.body(String.class);
+		return ordersFeignClient.helloOrders();
 	}
 	
 	@GetMapping
@@ -50,5 +58,13 @@ public class ProductController
 	public ResponseEntity<Product> getInventoryById(@PathVariable("id") Long id)
 	{
 		return new ResponseEntity<Product>(productService.getProductById(id), HttpStatus.OK);
+	}
+
+	
+	@PutMapping(path = "/reduce-stock")
+	public ResponseEntity<Double> reduceStocks(@RequestBody OrderRequest orderRequest)
+	{
+		Double totalPrice = productService.reduceStocks(orderRequest);
+		return ResponseEntity.ok(totalPrice);
 	}
 }
