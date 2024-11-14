0e4bed5f80c38bbfd6fdc523b5052990e8b231c8
ManPatel-07 <patelman7720@gmail.com>
2024-11-14 11:34:26 +0530

    Create book Service and store the who borrow the book ....
    Create book Service and store the who borrow the book ....

--- MicroServices_WorkSpace/inventory-service/src/main/java/org/mm/inventory_service/controller/ProductController.java
+++ MicroServices_WorkSpace/inventory-service/src/main/java/org/mm/inventory_service/controller/ProductController.java
@@ -3,21 +3,15 @@ package org.mm.inventory_service.controller;
 import java.util.List;
 
 import org.mm.inventory_service.clients.OrdersFeignClient;
-import org.mm.inventory_service.entity.OrderRequest;
 import org.mm.inventory_service.entity.Product;
 import org.mm.inventory_service.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -30,12 +24,6 @@ public class ProductController
 {
 	@Autowired
 	private ProductService productService;
-	
-	@Autowired
-	private DiscoveryClient discoveryClient;
-	
-	@Autowired
-	private RestClient restclient;
 	
 	@Autowired
 	private OrdersFeignClient ordersFeignClient;
@@ -60,11 +48,4 @@ public class ProductController
 		return new ResponseEntity<Product>(productService.getProductById(id), HttpStatus.OK);
 	}
 
-	
-	@PutMapping(path = "/reduce-stock")
-	public ResponseEntity<Double> reduceStocks(@RequestBody OrderRequest orderRequest)
-	{
-		Double totalPrice = productService.reduceStocks(orderRequest);
-		return ResponseEntity.ok(totalPrice);
-	}
 }
--- MicroServices_WorkSpace/inventory-service/src/main/java/org/mm/inventory_service/service/ProductService.java
+++ MicroServices_WorkSpace/inventory-service/src/main/java/org/mm/inventory_service/service/ProductService.java
@@ -1,10 +1,8 @@
 package org.mm.inventory_service.service;
 
-import java.util.Iterator;
+
 import java.util.List;
 
-import org.mm.inventory_service.entity.OrderRequest;
-import org.mm.inventory_service.entity.OrderRequestItem;
 import org.mm.inventory_service.entity.Product;
 import org.mm.inventory_service.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,17 +24,4 @@ public class ProductService
 		return productRepository.findById(id).orElseThrow();
 	}
 
-	public Double reduceStocks(OrderRequest orderRequest)
-	{
-		for(OrderRequestItem orderRequestItem: orderRequest.getItems())
-		{
-			Long productId = orderRequestItem.getProductId();
-			Integer qantity = orderRequestItem.getQantity();
-		
-			productRepository.findById(productId).orElseThrow(() -> 
-				new RuntimeException("Product NOt Found "));
-			
-		}
-		return null;
-	}
 }
