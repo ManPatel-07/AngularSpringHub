e26cded0c00d33a216e2c3b78b8e58e16b7b7673
ManPatel-07 <patelman7720@gmail.com>
2024-11-27 17:34:16 +0530

    Create a controller for the restaurant service
    Create a controller for the restaurant service

--- MicroFoodDelivery_WorkSpace/customer-service/src/main/java/org/mm/customer_service/controller/CustomerController.java
+++ MicroFoodDelivery_WorkSpace/customer-service/src/main/java/org/mm/customer_service/controller/CustomerController.java
@@ -3,9 +3,11 @@ package org.mm.customer_service.controller;
 import org.mm.customer_service.entity.Customer;
 import org.mm.customer_service.service.CustomerService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +37,18 @@ public class CustomerController
 	public ResponseEntity<?> getCustomerById(@PathVariable("id") Long id)
 	{
 		return ResponseEntity.ok(customerService.getCustomerById(id));
+	}
+	
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<?> updateCustomerDetails(@RequestBody Customer customer,
+												@PathVariable("id") Long id)
+	{
+		return ResponseEntity.ok(customerService.updateCustomerDetails(id, customer));
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> deleteCustomerById(@PathVariable("id") Long id)
+	{
+		return ResponseEntity.ok(customerService.deleteById(id));
 	}
 }
