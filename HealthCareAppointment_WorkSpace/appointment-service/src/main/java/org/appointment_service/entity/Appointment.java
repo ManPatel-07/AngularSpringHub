0a63a25aef079d4de008bc89294b730ba0668791
ManPatel-07 <patelman7720@gmail.com>
2024-12-12 12:12:04 +0530

    not do anytype of mapping in microservice
    not do anytype of mapping in microservice

--- HealthCareAppointment_WorkSpace/appointment-service/src/main/java/org/appointment_service/entity/Appointment.java
+++ HealthCareAppointment_WorkSpace/appointment-service/src/main/java/org/appointment_service/entity/Appointment.java
@@ -2,15 +2,11 @@ package org.appointment_service.entity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-
-import org.appointment_service.dao.Patient;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,12 +27,10 @@ public class Appointment
     private LocalDate appointmentDate;
     private LocalTime appointmentTime;
 
-    @ManyToOne
-    @JoinColumn(name = "patient_id")
-    private Patient patient;
+    @Column(name = "patient_id")
+    private Long patient;
 
-//    @ManyToOne
-//    @JoinColumn(name = "doctor_id")
-//    private Doctor doctor;
+    @Column(name = "doctor_id")
+    private Long doctor;
 
 }
