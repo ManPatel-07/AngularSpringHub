b942ef44188e263e244d990593cb8b13aa5ee41f
ManPatel <patelman7720@gmail.com>
2025-01-03 10:53:45 +0530

    Create Image store on the local storage and that image name in the db
    Create Image store on the local storage and that image name in the db

---
+++ ImageStore_WorkSpace/ImageStoreLocal/src/main/java/org/mm/ImageStoreLocalApplication.java
@@ -0,0 +1,13 @@
+package org.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStoreLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageStoreLocalApplication.class, args);
+	}
+
+}
