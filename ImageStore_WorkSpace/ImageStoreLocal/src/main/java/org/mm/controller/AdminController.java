c8d991a6c8a387853006998a172967413bb95333
ManPatel <patelman7720@gmail.com>
2025-01-03 11:34:21 +0530

    create a controller for the retrive the image data

--- ImageStore_WorkSpace/ImageStoreLocal/src/main/java/org/mm/controller/AdminController.java
+++ ImageStore_WorkSpace/ImageStoreLocal/src/main/java/org/mm/controller/AdminController.java
@@ -1,18 +1,32 @@
 package org.mm.controller;
 
 import org.mm.service.AdminService;
+import org.mm.service.utility.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 
 @RestController
 public class AdminController 
 {
 	@Autowired
 	private AdminService adminService;
+	
+    @Autowired
+    private FileUtils fileUtils;
 	
 	@PostMapping(value = "/addAdminData", consumes = "multipart/form-data")
 	public ResponseEntity<?> addOrUpdatepackageMaterial(@RequestParam("data") String json,
@@ -21,5 +35,29 @@ public class AdminController
 		return adminService.addAdminDetails(json, adminImage);
 		
 	}
+	
+	
+    @GetMapping("/image/{fileName}")
+    public ResponseEntity<?> getImage(@PathVariable String fileName) {
+        try {
+            File imageFile = fileUtils.getFile(fileName, "materialImages");
+
+            if (imageFile != null && imageFile.exists()) {
+                Path path = imageFile.toPath();
+                byte[] fileBytes = Files.readAllBytes(path);
+
+                String contentType = Files.probeContentType(path);
+
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(contentType))
+                        .body(fileBytes);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Image not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving image");
+        }
+    }
 
 }
