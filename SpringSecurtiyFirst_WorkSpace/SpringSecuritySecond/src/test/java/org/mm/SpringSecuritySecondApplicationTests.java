fe4f0ad427d2cb026e2f5e889a5f58d8fa6d2cbf
ManPatel <patelman7720@gmail.com>
2025-01-07 13:51:49 +0530

    create a method for the get id from the token in jwt service
    create a method for the get id from the token in jwt service

--- SpringSecurtiyFirst_WorkSpace/SpringSecuritySecond/src/test/java/org/mm/SpringSecuritySecondApplicationTests.java
+++ SpringSecurtiyFirst_WorkSpace/SpringSecuritySecond/src/test/java/org/mm/SpringSecuritySecondApplicationTests.java
@@ -1,13 +1,30 @@
 package org.mm;
 
 import org.junit.jupiter.api.Test;
+import org.mm.entity.SecondUserEntity;
+import org.mm.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSecuritySecondApplicationTests {
+	
+	@Autowired
+	private JwtService jwtService;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		SecondUserEntity user = new SecondUserEntity(2l, "test@gmail.com", "123321");
+		
+		String token = jwtService.generateToken(user);
+		
+		System.out.println(token);
+		
+		Long id = jwtService.getUserIdFromToken(token);
+		
+		System.out.println(id);
+		
 	}
 
 }
