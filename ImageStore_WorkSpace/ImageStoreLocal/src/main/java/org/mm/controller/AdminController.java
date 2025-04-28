358e964c5aea69fc056107ff6bd37746dc636782
ManPatel-07 <patelman7720@gmail.com>
2025-04-28 11:47:57 +0530

    Do some chanegs on the image retrive code
    Do some chanegs on the image retrive code

--- ImageStore_WorkSpace/ImageStoreLocal/src/main/java/org/mm/controller/AdminController.java
+++ ImageStore_WorkSpace/ImageStoreLocal/src/main/java/org/mm/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -44,7 +45,7 @@ public class AdminController
 	}
 	
     @GetMapping("/image/{fileName}")
-    public ResponseEntity<?> getImage(@PathVariable String fileName) {
+    public ResponseEntity<?> getImage(@PathVariable("fileName") String fileName) {
         try {
             File imageFile = fileUtils.getFile(fileName, "adminImage");
 
